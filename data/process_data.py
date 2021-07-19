f01814785942eebbf6c643e53bdce8bbb8389812
Hakan ATEŞLİ <hakaatesli9000@gmail.com>
2021-07-20 00:00:38 +0300

    Filled Process_data.py file

---
+++ data/process_data.py
@@ -0,0 +1,74 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    
+    """
+    messages = pd.read_csv(messages_filepath, delimiter=",", encoding = 'utf-8')
+    categories = pd.read_csv(categories_filepath,  delimiter=",", encoding = 'utf-8')
+    df = messages.merge(categories, how='inner', on= 'id')
+    return df
+
+
+def clean_data(df):
+    """
+    
+    """
+    categories = df['categories'].str.split(";", expand=True)
+    row = categories.iloc[0]
+    categories.columns = row.apply(lambda x: x[:-2])
+    
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x:1 if int(x[-1]) > 1 else int(x[-1]))
+
+    df.drop('categories', axis=1, inplace=True)
+
+    df = pd.concat([df, categories], axis=1)
+
+    df = df[df.duplicated()==False]
+    #df.drop_duplicates(inplace=True)
+
+    df = df[~(df.isnull().any(axis=1))|(df.original.isnull())]
+    
+    return df
+
+
+def save_data(df, database_filename):
+    """
+    
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, if_exists='replace', index=False) 
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
