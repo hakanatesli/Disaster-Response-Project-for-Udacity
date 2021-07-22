a50ffdfe985f25ab47b6567dc003942ace140b37
hakanatesli <35893541+hakanatesli@users.noreply.github.com>
2021-07-22 11:40:29 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,3 +1,14 @@
+"""
+TR: Bu dosya ile aşağıdaki işlemler yapılmaktadır:
+    -Veri Yükleme,
+    -Veriyi Temizleme,
+    -Veriyi DB'ye kayıt etme.
+
+EN: The following operations are performed with this file:
+    -Data Upload,
+    -Cleaning Data,
+    -Saving the data to the DB.
+"""
 import sys
 import pandas as pd
 import numpy as np
@@ -5,7 +16,20 @@ from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
     """
+    TR: Veriyi yükleme fonksiyonudur.
+        
+        GİRDİ: -messages_filepath: Mesajlar dosyası yolu
+               -categories_filepath: Kategoriler dosyası yolu
+               
+        ÇIKTI: Mesajlar ve Kategoriler dosyalarının birleştirilmiş hali
     
+    
+    EN: The function of loading data.
+        
+        INPUT: -messages_filepath: Path to Messages file
+               -categories_filepath: Path to categories file
+               
+        OUTPUT: Merged Messages and Categories files    
     """
     messages = pd.read_csv(messages_filepath, delimiter=",", encoding = 'utf-8')
     categories = pd.read_csv(categories_filepath,  delimiter=",", encoding = 'utf-8')
@@ -15,19 +39,31 @@ def load_data(messages_filepath, categories_filepath):
 
 def clean_data(df):
     """
-    
+    TR: Veriyi temizler.
+        
+        GİRDİ: Kirli DataFrame
+        
+        ÇIKTI: Temizlenmiş DataFrame
+        
+    EN: Cleaned the data.
+        
+         INPUT: Dirty DataFrame
+        
+         OUTPUT: Cleaned DataFrame
     """
+    #Separates the categories data with a semicolon. 
+    #Sets the first part of the allocated data to be the header and the second part to be data.
     categories = df['categories'].str.split(";", expand=True)
     row = categories.iloc[0]
     categories.columns = row.apply(lambda x: x[:-2])
     
     for column in categories:
         categories[column] = categories[column].apply(lambda x:1 if int(x[-1]) > 1 else int(x[-1]))
-
+    #Drop old categories column
     df.drop('categories', axis=1, inplace=True)
-
+    #Add new categories columns
     df = pd.concat([df, categories], axis=1)
-
+    #Removed Duplicated data, You can choose one of two options.
     df = df[df.duplicated()==False]
     #df.drop_duplicates(inplace=True)
 
@@ -38,7 +74,19 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     """
-    
+    TR: Temizlenmiş veriyi DB'ye kayıt eder.
+        
+        GİRDİ: df: Temizlenmiş DataFrame
+               database_filename: VeriTabanına verilecek isim
+        
+        ÇIKTI: Çıktı Yok.
+        
+    EN: Saves the cleaned data to the DB.
+        
+         INPUT: df: Cleaned DataFrame
+                database_filename: The name to be given to the database
+        
+         OUTPUT: No Output.
     """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('DisasterResponse', engine, if_exists='replace', index=False) 
